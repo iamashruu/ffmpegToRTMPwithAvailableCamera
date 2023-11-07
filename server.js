004b559d806417bb97bc0e5f8831306767ed1154
unknown <ashrafuddin882@gmail.com>
2023-11-07 13:29:29 +0600

    adding start & stop streaming of ffmpeg process

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { exec } = require("child_process");
 
 const app = express();
 const port = 4000;
+let ffmpegProcess;
 
-app.get("/startStream", (req, res) => {
-  const selectedCamera = req.query.camera;
+app.use(bodyParser.json()); // Add body-parser middleware
 
-  const ffmpegCommand = `ffmpeg -f dshow -i video="${selectedCamera}" -vf 'format=yuv420p' -c:v libx264 -tune zerolatency -preset veryfast -an -f flv rtmp://localhost:1935/live/camera/1`;
+app.use(express.static("public"));
 
-  const ffmpegProcess = exec(ffmpegCommand, (error, stdout, stderr) => {
+app.post("/cameralist", (req, res) => {
+  const CameraId = req.body.cameraId; // Assuming data is sent in the POST body
+  const CameraLabel = req.body.cameraLabel;
+  const CameraNumber = req.body.cameraNumber;
+
+  console.log(CameraId, CameraLabel); // Check the received data
+
+  // Rest of your code for executing the FFmpeg command with selectedCamera
+
+  const ffmpegCommand = `ffmpeg -f dshow -i video="${CameraLabel}" -c:v libx264 -tune zerolatency -preset veryfast -an -f flv rtmp://localhost:1935/live/camera/${CameraNumber}`;
+
+  ffmpegProcess = exec(ffmpegCommand, (error, stdout, stderr) => {
     if (error) {
       console.error("Error starting FFmpeg:", error);
       res.status(500).send("Error starting FFmpeg");
@@ -20,8 +32,28 @@ app.get("/startStream", (req, res) => {
   });
 });
 
-app.use(express.static("public")); // Serving HTML and client-side scripts from the 'public' directory
+const treeKill = require("tree-kill");
 
-app.listen(port, () => {
+app.post("/stopstream", (req, res) => {
+  console.log("Stopping FFmpeg...");
+
+  if (ffmpegProcess) {
+    treeKill(ffmpegProcess.pid, "SIGTERM", (err) => {
+      if (err) {
+        console.error("Error stopping FFmpeg:", err);
+        res.status(500).send("Error stopping FFmpeg.");
+      } else {
+        console.log("FFmpeg process stopped.");
+        ffmpegProcess = null;
+        res.send("Streaming stopped.");
+      }
+    });
+  } else {
+    console.log("No FFmpeg process running.");
+    res.status(500).send("No FFmpeg process running.");
+  }
+});
+
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
